ccf8f31b9dd415d8ce708c4e860bd844e8b33a78
Shreyansh <test@mail.com>
2025-08-23 23:34:28 +0530

    project

---
+++ BankingProject/project.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file project.cpp
+ * @author Shreyansh
+ * @brief Project: Bank Account System
+            Concepts covered:
+            Class & Object
+            Private data (encapsulation)
+            Public methods (deposit, withdraw, check balance)
+ * @date 2025-08-23
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+class BankAccount{
+  private:
+    string owner;
+    double balance;
+  public:
+    // Constructor
+    BankAccount(string name, int intialBalance){
+      owner = name;
+      balance = intialBalance>=0?intialBalance:0;
+    }
+
+    // Function to deposit money
+    void deposit(double amount){
+      if(amount>0){
+        balance+=amount;
+        cout<<"Deposited:"<<amount<<"| New Balance:"<<balance<<endl;
+      }else{
+        cout<<"Invalid amout";
+      }
+    }
+
+    // Function to withdraw money
+    void withdraw(double amount){
+      if(amount>0 && amount<=balance){
+        balance-=amount;
+        cout<<"Withdrawn:"<<amount<<"| New Balance:"<<balance<<endl;
+      }else{
+        cout<<"Insufficient amount";
+      }
+    }
+
+    // Show account
+    void showAccount(){
+      cout<<"Owner:"<<owner<<"| Account Balance:"<<balance<<endl;
+    }
+};
+
+int main(){
+  BankAccount acc("Emma",10000);
+
+  acc.showAccount();
+  acc.deposit(500);
+  acc.withdraw(1000);
+  acc.deposit(2000);
+  acc.showAccount();
+}
